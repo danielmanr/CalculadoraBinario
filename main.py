16baa5a97dea6ddd572bcb6801198dfabc8aea5c
danielmanr <forigua.daniel@gmail.com>
2022-08-17 00:09:34 -0500

    Decimal a Hexadecimal ok

--- main.py
+++ main.py
@@ -3,8 +3,44 @@ def menu():
     print("*****Bienvenido*****")
     print("1. Decimal a Binario")
     print("2. Binario a Decimal")
+    print("3. Decimal a Hexadecimal")
     opc = input("Digite una Opcion:\n")
     return opc
+
+
+def decimalAHexadecimal():
+    decimal = int(input("Ingrese el decimal a convertir:\n"))
+    hexa = []
+    resultado = ""
+    while decimal >= 1:
+        residuo = decimal % 16
+        hexa.append(residuo)
+        decimal = int(decimal / 16)
+    impresionHexa = hexa[::-1]
+    for i in impresionHexa:
+        i = str(i)
+        if i == "10":
+            resultado = resultado + "A"
+            continue
+        elif i == "11":
+            resultado = resultado + "B"
+            continue
+        elif i == "12":
+            resultado = resultado + "C"
+            continue
+        elif i == "13":
+            resultado = resultado + "D"
+            continue
+        elif i == "14":
+            resultado = resultado + "E"
+            continue
+        elif i == "15":
+            resultado = resultado + "F"
+            continue
+        else:
+            resultado = resultado + i
+    print(resultado)
+
 
 def binarioADecimal():
     decimal = 0
@@ -13,19 +49,12 @@ def binarioADecimal():
     invertidoBinario = binario[::-1]
     for i in invertidoBinario:
         if i == "1":
-            if cont >= 1:
-                decimal += 2**cont
-                cont += 1
-                continue
             decimal += 2**cont
             cont += 1
         elif i == "0":
             cont += 1
             continue
     print("su numero en decimal es:\n" + str(decimal))
-    print("********"+"1111111")
-
-
 
 
 def decimalABinario():
@@ -45,18 +74,14 @@ def decimalABinario():
     print("Su numero binario es:\n "+ vlr)
 
 
-
-
-
 def run():
     opcion = menu()
     if opcion == "1":
         decimalABinario()
     elif opcion == "2":
         binarioADecimal()
-
-
-
+    elif opcion == "3":
+        decimalAHexadecimal()
 
 
 if __name__ == '__main__':
